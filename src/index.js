3e4d05cd51de5ce30e26fadcb777a083e5ff5e50
Himanshu21994 <himanshukumar21994@gmail.com>
2025-10-08 00:07:08 +0530

    Updated files after moving folder and made code changes

--- src/index.js
+++ src/index.js
@@ -2,14 +2,25 @@
 import  dotenv from "dotenv";
 
 import mongoose from "mongoose";
-// import { DB_NAME } from "./constants";
 import connectDB from "./db/index.js";
 
 dotenv.config({
     path: './.env'
 });
 
-connectDB();
+connectDB()
+.then(()=> {
+    app.on("error", (err) => {
+        console.error("ERROR: ", err);
+        throw err;
+    });
+    app.listen(process.env.PORT || 8000, ()=> {
+        console.log(`Server started at http://localhost:${process.env.PORT || 8000}`);
+    });
+})
+.catch((err) => {
+    console.log("MongoDB connection error: ", err);
+});
 
 
 
